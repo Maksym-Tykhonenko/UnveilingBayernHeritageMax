cc9c4685701421c1f1a75a96b482fa4318e7f08d
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2025-07-08 18:40:15 +0300

    sss

--- StackNavigator.js
+++ StackNavigator.js
@@ -584,7 +584,7 @@ const AppNavigator = () => {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2025-07-01T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2025-07-11T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (!route) {
